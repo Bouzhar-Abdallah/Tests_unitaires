e353cf70fadbfaec089fdb6fae21546df1128fd3
abdallah <abddoullah.bouzhar@gmail.com>
2023-11-04 15:11:47 +0100

    Mockito to test create and delete

--- src/test/java/com/majidim/easybankv4/easybankv4/newService/DemandeCreditServiceTest.java
+++ src/test/java/com/majidim/easybankv4/easybankv4/newService/DemandeCreditServiceTest.java
@@ -8,9 +8,16 @@ import com.majidim.easybankv4.easybankv4.dto.Agence;
 import com.majidim.easybankv4.easybankv4.dto.Client;
 import com.majidim.easybankv4.easybankv4.dto.DemendeCredit;
 import com.majidim.easybankv4.easybankv4.dto.Employe;
+import com.majidim.easybankv4.easybankv4.exception.ParametreException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -19,27 +26,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DemandeCreditServiceTest {
 
-    private static DemandeCreditService demandeCreditService;
-    private static Employe employe;
-    private static Agence agence;
-    private static Client client;
+    DemandeCreditService demandeCreditService;
+    @Mock
+    DemandeCreditImpl impDemandeCredit;
+    Employe employe;
+    Agence agence;
+    Client client;
 
-    @BeforeAll
-    public static void setUp(){
-        demandeCreditService = new DemandeCreditService(new DemandeCreditImpl());
-        EmployeImpl employeImp = new EmployeImpl();
-        AgenceImp agenceImp = new AgenceImp();
-        ClientImpl clientImp = new ClientImpl();
-        Optional<Client> optionalClient = clientImp.findByID("client");
-        Optional<Employe> employeOptional = employeImp.findByID("employer");
-        Optional<Agence> agenceOptional = agenceImp.findByID("agence");
-        optionalClient.ifPresent(value -> client = value);
-        employeOptional.ifPresent(value -> employe = value);
-        agenceOptional.ifPresent(value -> agence = value);
-
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        demandeCreditService = new DemandeCreditService(impDemandeCredit);
     }
+
     @Test
     void createTrue() {
+        DemendeCredit demendeCreditTrue = createValidDemendeCredit();
+
+        // Mock the service
+        when(impDemandeCredit.create(demendeCreditTrue)).thenReturn(Optional.of(demendeCreditTrue));
+
+        // Act
+        Optional<DemendeCredit> result = demandeCreditService.create(demendeCreditTrue);
+
+        // Assert that the result is present and contains the expected data
+        assertTrue(result.isPresent(), "The creation should be successful");
+        assertEquals(demendeCreditTrue, result.get(), "The created object should match the expected object");
+        verify(impDemandeCredit, Mockito.times(1)).create(demendeCreditTrue);
+    }
+
+    @Test
+    void createFalse() {
+        DemendeCredit demendeCreditFalse = createInvalidDemendeCredit();
+
+        Optional<DemendeCredit> result = demandeCreditService.create(demendeCreditFalse);
+
+        assertTrue(result.isEmpty());
+        verify(impDemandeCredit, Mockito.times(0)).create(demendeCreditFalse);
+
+    }
+
+    @Test
+    void createFalseException() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            demandeCreditService.create(null);
+        });
+    }
+
+    @Test
+    void testDeleteWithException() {
+
+        String demandeCreditId = "";
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, () -> demandeCreditService.delete(demandeCreditId),
+                "Delete should throw ParametreException");
+    }
+    @Test
+    void testDeleteNoException() {
+
+        String demandeCreditId = "x";
+
+        when(impDemandeCredit.delete(demandeCreditId)).thenReturn(true);
+
+        boolean result = demandeCreditService.delete(demandeCreditId);
+        verify(impDemandeCredit, Mockito.times(1)).delete(demandeCreditId);
+        assertTrue(result);
+
+    }
+
+    private DemendeCredit createValidDemendeCredit() {
         DemendeCredit demendeCreditTrue = new DemendeCredit();
         demendeCreditTrue.setNumero("xxx");
         demendeCreditTrue.setMontant(5000D);
@@ -52,11 +109,10 @@ class DemandeCreditServiceTest {
         demendeCreditTrue.setEmploye(employe);
         demendeCreditTrue.setAgence(agence);
         demendeCreditTrue.setClient(client);
-        Optional<DemendeCredit> optionalDemendeCredit = demandeCreditService.create(demendeCreditTrue);
-        assertTrue(optionalDemendeCredit.isPresent());
+        return demendeCreditTrue;
     }
-    @Test
-    void createFalse(){
+
+    private DemendeCredit createInvalidDemendeCredit() {
         //create demende credit not ok
         DemendeCredit demendeCreditFalse = new DemendeCredit();
         demendeCreditFalse.setNumero("xxxx");
@@ -70,13 +126,7 @@ class DemandeCreditServiceTest {
         demendeCreditFalse.setEmploye(employe);
         demendeCreditFalse.setAgence(agence);
         demendeCreditFalse.setClient(client);
+        return demendeCreditFalse;
+    }
 
-        Optional<DemendeCredit> optionalDemendeCredit = demandeCreditService.create(demendeCreditFalse);
-        assertFalse(optionalDemendeCredit.isPresent());
-    }
-    @AfterAll
-    public static void tearDown (){
-        demandeCreditService.delete("xxx");
-        demandeCreditService.delete("xxxx");
-    }
 }
